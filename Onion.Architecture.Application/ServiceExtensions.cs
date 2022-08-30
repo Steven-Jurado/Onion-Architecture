180bd2c9fb6400e4808b780c5db18503d2fae333
Steven Jurado <steven.jurado@guru-soft.com>
2022-08-30 09:55:18 -0500

    Se Agregon Paquetes Necesarios Para Aplicar Patron CQRS, Mediator

---
+++ Onion.Architecture.Application/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Onion.Architecture.Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddAplicationLayer( this IServiceCollection services)
+        {
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        
+        }
+    }
+}
