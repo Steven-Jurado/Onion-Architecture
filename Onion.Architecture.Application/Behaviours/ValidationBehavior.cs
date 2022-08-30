f6a00e7df9da7ec15be5ae689a4384068abc565a
Steven Jurado <steven.jurado@guru-soft.com>
2022-08-30 10:35:54 -0500

    Se agrego Pipeline Para Validacion de Request En Capa De Aplicacion

---
+++ Onion.Architecture.Application/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Onion.Architecture.Application.Behaviours
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(_validators.Select(x => x.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResults.SelectMany(x => x.Errors).Where(x => x is not null).AsEnumerable();
+
+                if (failures.Any())
+                    throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
---
+++ Onion.Architecture.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+
+namespace Onion.Architecture.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IEnumerable<string> Erros { get; }
+
+        public ValidationException() : base("Se han producido uno o más error de validación")
+        {
+            Erros = new List<string>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Erros.Append(failure.ErrorMessage);
+            }
+        }
+
+    }
+}
